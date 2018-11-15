00ddb7b0f2c9728584b47a1821258485c47524b5
iwanttobealight <iwanttobealight@yandex.ru>
2018-11-15 07:36:43 +0300

    finished frontend

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,29 @@
 const button = document.querySelector('.main-button');
 const catalog = document.querySelector('.catalog');
+let count = 0;
 
-const addPhoto = function() {
+const MYSTIC_CONDITION = function (num) {
+  return num > 2
+}
+
+const increase = function (num) {
+  return num += 1;
+}
+
+const mysticFunction = function (num) {
+  if (MYSTIC_CONDITION(num)) {
+    document.location.reload();
+  }
+}
+
+const addPhoto = function () {
   const photo = document.createElement('img');
   photo.src = 'http://placecorgi.com/100/100';
-  catalog.appendChild(photo); 
+  catalog.appendChild(photo);
+
+  count = increase(count);
+
+  mysticFunction(count);
 }
 
 
